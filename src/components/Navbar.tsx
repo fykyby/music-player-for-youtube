05f80d0d2a1e418f29ee130211146172b8485a09
fykyby <iploritpl@gmail.com>
2022-09-01 19:17:21 +0200

    Highlight selected page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,29 @@
 import styles from "../styles/Navbar.module.css";
 import miscStyles from "../styles/misc.module.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { Search, MusicNoteList } from "react-bootstrap-icons";
 
 export default function Navbar(): JSX.Element {
+  const location = useLocation();
+  console.log(location.pathname);
+
   return (
     <div className={styles.Navbar}>
       <Link to="/playlist">
-        <div className={styles.link}>
+        <div
+          className={`${styles.link} ${
+            location.pathname === "/playlist" ? styles.active : null
+          }`}
+        >
           <MusicNoteList className={miscStyles.smallIcon} />
         </div>
       </Link>
       <Link to="/search">
-        <div className={styles.link}>
+        <div
+          className={`${styles.link} ${
+            location.pathname === "/search" ? styles.active : null
+          }`}
+        >
           <Search className={miscStyles.smallIcon} />
         </div>
       </Link>
