a31f04de7ac9d97a60a8a1ea658d21f51be7e2f0
fykyby <iploritpl@gmail.com>
2022-09-20 18:02:05 +0200

    Style tweaks

--- src/components/PlaylistSong.tsx
+++ src/components/PlaylistSong.tsx
@@ -26,6 +26,7 @@ export default function PlaylistSong({
 
   async function handleClick(e: any) {
     e.preventDefault();
+    console.log(data);
     if (currentSource?.index === data.index) return;
     setCurrentSongIndex(data.index);
   }
