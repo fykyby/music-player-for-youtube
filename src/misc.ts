be12d177decd41cfb897f23aa8658aade69b5bb9
fykyby <iploritpl@gmail.com>
2022-10-26 22:57:20 +0200

    Restore last commit

--- src/misc.ts
+++ src/misc.ts
@@ -43,7 +43,7 @@ export async function getPlaylistVideos(id: string) {
         return newItem;
       });
 
-      newPlaylist = [...finalPlaylistData];
+      newPlaylist = [...newPlaylist, ...finalPlaylistData];
     } while (nextPageToken);
 
     return newPlaylist;
