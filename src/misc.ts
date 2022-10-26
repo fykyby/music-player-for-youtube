d33d6d35aa6688d753182276824b31a4fb9582fe
fykyby <iploritpl@gmail.com>
2022-10-26 22:36:09 +0200

    Remove useless array spread

--- src/misc.ts
+++ src/misc.ts
@@ -43,7 +43,7 @@ export async function getPlaylistVideos(id: string) {
         return newItem;
       });
 
-      newPlaylist = [...newPlaylist, ...finalPlaylistData];
+      newPlaylist = [...finalPlaylistData];
     } while (nextPageToken);
 
     return newPlaylist;
